9c027035eeec18786bb8c1ca4ec378242908e629
Giampaolo Tranchida <giampaolo.tranchida@vd.ch>
2015-06-15 17:01:44 +0200

    Initial commit

---
+++ package.js
@@ -0,0 +1,20 @@
+Package.describe({
+  name: 'tranchida:primeui',
+  version: '1.0.0',
+  // Brief, one-line summary of the package.
+  summary: 'add PrimeFaces PrimeUI 2.0 minified js and css to meteor project',
+  // URL to the Git repository containing the source code for this package.
+  git: '',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.1.0.2');
+  api.use('jquery', 'client');
+  api.use('mizzao:jquery-ui', 'client');
+  api.addFiles('primeui-2.0-min.css', 'client');
+  api.addFiles('primeui-2.0-min.js', 'client');
+});
+
